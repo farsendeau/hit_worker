c5eb199af0b02a30e5d4477a60acf3b173b0d9af
karigane <karigane@KARI-DESKTOP.>
2026-01-02 14:36:10 +0100

    fix ladder

--- src/level/Level.cpp
+++ src/level/Level.cpp
@@ -17,10 +17,8 @@ bool Level::isSolidAt(int tileX, int tileY) const
 
 bool Level::isLadderAt(int tileX, int tileY) const
 {
-    // TODO: Implémenter quand on aura les échelles dans le tileset
-    (void)tileX;
-    (void)tileY;
-    return false;
+    // Utilise les fonctions de level1Data.h
+    return ::isLadderAt(tileX, tileY);  // :: pour utiliser la fonction globale
 }
 
 bool Level::isKillAt(int tileX, int tileY) const
